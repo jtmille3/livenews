f39ae05121ca54e16075e27c3ab78d591dad8e8a
Jeff Miller <jeffmiller@jeffmiller.name>
2013-12-31 15:11:45 -0500

    notes on rotation

--- app/scripts/entities/Pin.js
+++ app/scripts/entities/Pin.js
@@ -17,18 +17,18 @@ define([
         })
       );
 
-      // calculate drop coordinates based on passed long and lat...
-      ball.position.z += 0.505;
+      ball.position.z += 0.505; // plant the ball 0.5 up from earth and 0.005 up to reveal the entire pin
 
       this.pin.add(ball);
 
-      // this.pin.rotation.x = this.options.latitude * Math.PI / -180; // 35 degrees
-      // this.pin.rotation.y = this.options.longitude * Math.PI / 180; // -74 degrees
-
+      // rotate from the base of the world
       var quaternion= new THREE.Quaternion();
+
+      // translate along the latitude or x-axis (make negative to reverse spin)
       quaternion.setFromAxisAngle(new THREE.Vector3(1,0,0).normalize(), this.options.latitude * Math.PI / -180);
       this.pin.quaternion.multiplyQuaternions(quaternion, this.pin.quaternion);
 
+      // translate along the longitude or y-axis
       quaternion.setFromAxisAngle(new THREE.Vector3(0,1,0).normalize(), this.options.longitude * Math.PI / 180);
       this.pin.quaternion.multiplyQuaternions(quaternion, this.pin.quaternion);
 
